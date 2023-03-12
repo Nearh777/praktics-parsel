6ca6a72bd1d4045709f01f5ef559d9af5049d393
Дмитрий Хоменко <82578557+choodoss@users.noreply.github.com>
2023-03-12 12:16:36 +0200

    upate

--- src/js/form.js
+++ src/js/form.js
@@ -1,21 +1,33 @@
+import storageApi from './storage.js';
+
 const form = document.querySelector('.js-contact-form');
 const STORAGE_KEY = 'user-data-key';
 initPage();
 form.addEventListener('input', handlerInput);
 
 function handlerInput(e) {
-  const savedData = JSON.parse(localStorage.getItem(STORAGE_KEY)) || {};
-
+  const savedData = storageApi.load(STORAGE_KEY) || {};
   savedData[e.target.name] = e.target.value;
-  localStorage.setItem(STORAGE_KEY, JSON.stringify(savedData));
+  storageApi.save(STORAGE_KEY, savedData)
 }
 
 function initPage() {
-  const savedData = JSON.parse(localStorage.getItem(STORAGE_KEY));
-  console.log(savedData);
+  const savedData = storageApi.load(STORAGE_KEY);
   if (savedData) {
     Object.entries(savedData).forEach(([name, value]) => {
       form.elements[name].value = value;
     });
   }
 }
+
+function handlerSaveData(e) {
+  e.preventDefault();
+  const userData = {};
+  const formData = new FormData(form);
+  formData.forEach((value, name) => userData[name] = value);
+  console.log(userData);
+  storageApi.remove(STORAGE_KEY);
+  e.target.reset();
+}
+
+form.addEventListener('submit', handlerSaveData);
