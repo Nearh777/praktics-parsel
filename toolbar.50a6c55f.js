bb3bc7734e6d224fc11ff470a93df5034f3b78bd
choodoss <82578557+choodoss@users.noreply.github.com>
2023-03-12 10:53:38 +0000

    Deploying to gh-pages from @ Nearh777/praktics-parsel@88030be4319dc185873044488f45aae17e8603e2 🚀

---
+++ toolbar.50a6c55f.js
@@ -0,0 +1,2 @@
+document.querySelector(".theme-switch__toggle").addEventListener("change",(function(e){console.log(e.target.checked)}));
+//# sourceMappingURL=toolbar.50a6c55f.js.map
---
+++ toolbar.c2384c88.js
@@ -0,0 +1,2 @@
+document.querySelector(".theme-switch__toggle").addEventListener("change",(function(e){console.log(e.target.checked)}));
+//# sourceMappingURL=toolbar.c2384c88.js.map
