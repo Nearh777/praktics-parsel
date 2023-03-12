ccef7259a219b648baac93725057c924ea3b084a
Denys Denysiuk <denya5111@gmail.com>
2023-03-12 10:47:12 +0100

    dla Dmytra ))

--- src/js/form.js
+++ src/js/form.js
@@ -1,12 +1,21 @@
 const form = document.querySelector('.js-contact-form');
 const STORAGE_KEY = 'user-data-key';
+initPage();
 form.addEventListener('input', handlerInput);
-const userData = {};
-function handlerInput (e){
-  //console.log(e.target.value);
-  
-  userData[e.target.name] =e.target.value;
-  localStorage.setItem(STORAGE_KEY, JSON.stringify(userData));
-  console.log(userData);
-}; 
 
+function handlerInput(e) {
+  const savedData = JSON.parse(localStorage.getItem(STORAGE_KEY)) || {};
+
+  savedData[e.target.name] = e.target.value;
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(savedData));
+}
+
+function initPage() {
+  const savedData = JSON.parse(localStorage.getItem(STORAGE_KEY));
+  console.log(savedData);
+  if (savedData) {
+    Object.entries(savedData).forEach(([name, value]) => {
+      form.elements[name].value = value;
+    });
+  }
+}
