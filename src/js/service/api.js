00cd49ba7d1ba404bd16d23848e8f9182e8b5c8e
Дмитрий Хоменко <82578557+choodoss@users.noreply.github.com>
2023-03-26 10:42:00 +0300

    add file photo

---
+++ src/js/service/api.js
@@ -0,0 +1,3 @@
+function searchData(query, page) {
+    // <https://api.unsplash.com/search/photos?page=1&query=office>; rel="first", <https://api.unsplash.com/search/photos?page=1&query=office>; rel="prev", <https://api.unsplash.com/search/photos?page=3&query=office>; rel="last", <https://api.unsplash.com/search/photos?page=3&query=office>; rel="next"
+}
